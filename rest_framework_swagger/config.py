6a6ed8f4fe3d3509eb926a3709038cf38cc9d1cf
Ryan Wooden <rygwdn@gmail.com>
2015-12-14 15:05:40 -0400

    Make swagger config errors more obvious.

--- rest_framework_swagger/config.py
+++ rest_framework_swagger/config.py
@@ -16,12 +16,15 @@ class SwaggerConfig(object):
     def __init__(self):
         super(SwaggerConfig, self).__init__()
         self.global_settings = self.DEFAULT_SWAGGER_SETTINGS.copy()
-        self.global_settings.update(settings.SWAGGER_GLOBAL_SETTINGS)
+        self.global_settings.update(getattr(settings, 'SWAGGER_GLOBAL_SETTINGS', {}))
 
     def get_config(self, config_name=None):
         config_name = config_name or "default"
+        if not hasattr(settings, 'SWAGGER_LOCAL_SETTINGS'):
+            raise Exception("SWAGGER_LOCAL_SETTINGS not configured in settings")
         if config_name not in settings.SWAGGER_LOCAL_SETTINGS:
-            raise Exception("{} swagger settings not defined".format(config_name))
+            raise Exception("{} swagger settings not defined in SWAGGER_LOCAL_SETTINGS".format(config_name))
+
         current_config = self.global_settings.copy()
         current_config.update(settings.SWAGGER_LOCAL_SETTINGS[config_name])
         return current_config
